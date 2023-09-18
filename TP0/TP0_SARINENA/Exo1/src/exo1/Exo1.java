6125020dcb1c78a7c2a586faf94f11473ddd80c4
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-09-18 11:14:24 +0200

    création d'un nouveau projet

---
+++ TP0/TP0_SARINENA/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,29 @@
+/*
+ SARINENA, TDC , calculatrice, 18/09/2023
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pierr
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        int i;
+        i=20;
+        i=i+1;
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc =  new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+}
