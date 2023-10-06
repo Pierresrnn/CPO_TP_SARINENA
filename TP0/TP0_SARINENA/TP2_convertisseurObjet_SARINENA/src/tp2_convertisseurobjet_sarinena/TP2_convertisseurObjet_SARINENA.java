906d3eb7e289cb00bb319bab8e54982aa42318d4
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-10-06 18:48:00 +0200

    convertisserurObjet

---
+++ TP0/TP0_SARINENA/TP2_convertisseurObjet_SARINENA/src/tp2_convertisseurobjet_sarinena/TP2_convertisseurObjet_SARINENA.java
@@ -0,0 +1,19 @@
+/*
+SARINENA, TDC , convetisseurObjet, 26/09/2023
+ */
+package tp2_convertisseurobjet_sarinena;
+
+/**
+ *
+ * @author pierr
+ */
+public class TP2_convertisseurObjet_SARINENA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
