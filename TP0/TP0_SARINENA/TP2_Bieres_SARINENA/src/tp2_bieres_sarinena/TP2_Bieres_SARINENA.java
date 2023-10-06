a7a50eaa28e2583d4e7bd26aaaf8398c6690e8d9
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-10-06 18:44:50 +0200

    bieres fin

--- TP0/TP0_SARINENA/TP2_Bieres_SARINENA/src/tp2_bieres_sarinena/TP2_Bieres_SARINENA.java
+++ TP0/TP0_SARINENA/TP2_Bieres_SARINENA/src/tp2_bieres_sarinena/TP2_Bieres_SARINENA.java
@@ -13,14 +13,29 @@ public class TP2_Bieres_SARINENA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls", 7.0, "Dubuisson" );
         uneBiere.lireEtiquette();
-        BouteilleBiere deuxBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
-        deuxBiere.lireEtiquette();        
-            
+        uneBiere.Décapsuler();
+        System.out.println(uneBiere) ;
         
+        BouteilleBiere deuxBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
+        deuxBiere.lireEtiquette();
+        System.out.println(deuxBiere) ;
         
-      
+        BouteilleBiere troisBiere = new BouteilleBiere("Meteor", 5.4, "Metzger Haag");
+        troisBiere.lireEtiquette();
+        troisBiere.Décapsuler();
+        System.out.println(troisBiere) ;
+        
+        BouteilleBiere quatreBiere = new BouteilleBiere("Desperados", 5.1, "Fischer");
+        quatreBiere.lireEtiquette();
+        quatreBiere.Décapsuler();
+        System.out.println(quatreBiere) ;
+        
+        BouteilleBiere cinqBiere = new BouteilleBiere("Kronenbourg", 4.2, "Calsberg");
+        cinqBiere.lireEtiquette();
+        System.out.println(cinqBiere) ;
+
     }
 
 }
