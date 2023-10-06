dacd707f04c7b9d34588f9f89325dbde708f0922
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-10-06 12:59:18 +0200

    bieres debut

---
+++ TP0/TP0_SARINENA/TP2_Bieres_SARINENA/src/tp2_bieres_sarinena/TP2_Bieres_SARINENA.java
@@ -0,0 +1,19 @@
+/*
+SARINENA, TDC , Bieres, 06/10/2023
+ */
+package tp2_bieres_sarinena;
+
+/**
+ *
+ * @author pierr
+ */
+public class TP2_Bieres_SARINENA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
