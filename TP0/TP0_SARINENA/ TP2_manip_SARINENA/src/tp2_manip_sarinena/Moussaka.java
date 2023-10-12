d1ecf58d5db5e8e1b90cff2db5096731d3c5f3eb
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-10-12 15:07:41 +0200

    manip milieu

--- TP0/TP0_SARINENA/ TP2_manip_SARINENA/src/tp2_manip_sarinena/Moussaka.java
+++ TP0/TP0_SARINENA/ TP2_manip_SARINENA/src/tp2_manip_sarinena/Moussaka.java
@@ -2,10 +2,16 @@
 SARINENA, TDC , manip, 12/10/2023
  */
 package tp2_manip_sarinena;
+
 /**
  *
  * @author pierr
  */
 public class Moussaka {
-    
+
+    int nbCalories;
+
+    public Moussaka(int nbCalories) {
+        this.nbCalories = nbCalories;
+    }
 }
