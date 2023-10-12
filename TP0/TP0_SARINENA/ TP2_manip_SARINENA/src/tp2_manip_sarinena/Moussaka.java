5d97f7e9c8c1694538ba571fa3ad39e043a94338
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-10-12 14:54:27 +0200

    manip debut

---
+++ TP0/TP0_SARINENA/ TP2_manip_SARINENA/src/tp2_manip_sarinena/Moussaka.java
@@ -0,0 +1,11 @@
+/*
+SARINENA, TDC , manip, 12/10/2023
+ */
+package tp2_manip_sarinena;
+/**
+ *
+ * @author pierr
+ */
+public class Moussaka {
+    
+}
---
+++ TP0/TP0_SARINENA/ TP2_manip_SARINENA/src/tp2_manip_sarinena/TP2_manip_SARINENA.java
@@ -0,0 +1,17 @@
+/*
+SARINENA, TDC , manip, 12/10/2023
+ */
+package tp2_manip_sarinena;
+/**
+ *
+ * @author pierr
+ */
+public class TP2_manip_SARINENA {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
