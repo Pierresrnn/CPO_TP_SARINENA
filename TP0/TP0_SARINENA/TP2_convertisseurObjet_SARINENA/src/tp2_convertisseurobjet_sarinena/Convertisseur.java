1b5d92ab3158ed80b7c497b239577776185b32da
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-10-12 14:22:28 +0200

    convertisserurObjet milieu

---
+++ TP0/TP0_SARINENA/TP2_convertisseurObjet_SARINENA/src/tp2_convertisseurobjet_sarinena/Convertisseur.java
@@ -0,0 +1,51 @@
+/*
+SARINENA, TDC , convetisseurObjet, 12/10/2023
+ */
+package tp2_convertisseurobjet_sarinena;
+
+import static tp2_convertisseurobjet_sarinena.TP2_convertisseurObjet_SARINENA.KelvinVersCelcius;
+
+
+public class Convertisseur {
+
+    int nbConversions;
+
+    public Convertisseur() {
+        nbConversions = 0;
+    }
+
+    public double CelciusVersKelvin(double tCelcius) {
+        nbConversions = 1 + nbConversions;
+        return tCelcius + 273.15;
+    }
+
+    public double KelvinVersCelcius(double tKelvin) {
+        nbConversions = 1 + nbConversions;
+        return tKelvin - 273.15;
+    }
+
+    public double FarenheitVersCelcius(double tFarenheit) {
+        nbConversions = 1 + nbConversions;
+        return (tFarenheit - 32) * 5 / 9;
+    }
+
+    public double CelciusVersFarenheit(double tCelcius) {
+        nbConversions = 1 + nbConversions;
+        return (tCelcius * 9 / 5) + 32;
+    }
+
+    public double KelvinVersFarenheit(double tKelvin) {
+        nbConversions = 1 + nbConversions;
+        return KelvinVersCelcius(CelciusVersFarenheit(tKelvin));
+    }
+
+    public double FarenheitVersKelvin(double tFarenheit) {
+        nbConversions = 1 + nbConversions;
+        return FarenheitVersCelcius(CelciusVersKelvin(tFarenheit));
+    }
+
+    @Override
+    public String toString() {
+        return "nb de conversions" + nbConversions;
+    }
+}
--- TP0/TP0_SARINENA/TP2_convertisseurObjet_SARINENA/src/tp2_convertisseurobjet_sarinena/TP2_convertisseurObjet_SARINENA.java
+++ TP0/TP0_SARINENA/TP2_convertisseurObjet_SARINENA/src/tp2_convertisseurobjet_sarinena/TP2_convertisseurObjet_SARINENA.java
@@ -1,7 +1,9 @@
 /*
-SARINENA, TDC , convetisseurObjet, 26/09/2023
+SARINENA, TDC , convetisseurObjet, 12/10/2023
  */
 package tp2_convertisseurobjet_sarinena;
+
+import java.util.Scanner;
 
 /**
  *
@@ -9,11 +11,68 @@ package tp2_convertisseurobjet_sarinena;
  */
 public class TP2_convertisseurObjet_SARINENA {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static double CelciusVersKelvin(double tCelcius) {
+        double tKelvin = tCelcius + 273.15;
+        return tKelvin;
     }
-    
+
+    public static double KelvinVersCelcius(double tKelvin) {
+        double tCelcius = tKelvin - 273.15;
+        return tCelcius;
+    }
+
+    public static double FarenheitVersCelcius(double tFarenheit) {
+        double tCelcius = (tFarenheit - 32) * 5 / 9;
+        return tCelcius;
+    }
+
+    public static double CelciusVersFarenheit(double tCelcius) {
+        double tFarenheit = (tCelcius * 9 / 5) + 32;
+        return tFarenheit;
+    }
+
+    public static double KelvinVersFarenheit(double tKelvin) {
+        double tCelcius = KelvinVersCelcius(tKelvin);
+        double tFarenheit = CelciusVersFarenheit(tCelcius);
+        return tFarenheit;
+    }
+
+    public static double FarenheitVersKelvin(double tFarenheit) {
+        double tCelcius = FarenheitVersCelcius(tFarenheit);
+        double tKelvin = CelciusVersKelvin(tCelcius);
+        return tKelvin;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bonjour, saisissez une valeur");
+        double reel = sc.nextDouble();
+        System.out.println("la valeure reelle entrée est :" + reel);
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer :1)De Celcius Vers Kelvin \n 2) De Kelvin Vers Celcius \n 3) De Farenheit Vers Celcius \n 4) De Celcius Vers Farenheit \n 5) De Kelvin Vers Farenheit \n 6) De Farenheit Vers Kelvin");
+        int choix = sc.nextInt();
+        while (choix < 1 || choix > 6) {
+            System.out.println("resaisissez un nombre valide:");
+            choix = sc.nextInt();
+        }
+        double resultat;
+        if (choix == 1) {
+            resultat = CelciusVersKelvin(reel);
+            System.out.println("la convertion donne " + resultat + "Kelvin");
+        } else if (choix == 2) {
+            resultat = KelvinVersCelcius(reel);
+            System.out.println("la convertion donne " + resultat + " Celcius");
+        } else if (choix == 3) {
+            resultat = FarenheitVersCelcius(reel);
+            System.out.println("la convertion donne " + resultat + " Celcius");
+        } else if (choix == 4) {
+            resultat = CelciusVersFarenheit(reel);
+            System.out.println("la convertion donne " + resultat + " Farenheit");
+        } else if (choix == 5) {
+            resultat = KelvinVersFarenheit(reel);
+            System.out.println("la convertion donne " + resultat + " Farenheit");
+        } else if (choix == 6) {
+            resultat = FarenheitVersKelvin(reel);
+            System.out.println("la convertion donne " + resultat + " Kelvin");
+        }
+    }
 }
