03a4a2af10edd036a9a5b50f5d4d1435d76a4114
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-10-23 14:57:24 +0200

    Heroic fantasy milieu

--- TP0/TP0_SARINENA/TP3_Heroic_Fantasy_SARINENA/src/Armes/Epee.java
+++ TP0/TP0_SARINENA/TP3_Heroic_Fantasy_SARINENA/src/Armes/Epee.java
@@ -1,7 +1,7 @@
 /*
 SARINENA, TDC , HEROIC FANTASY, 18/10/2023
  */
-package tp3_heroic_fantasy_sarinena;
+package Armes;
 
 /**
  *
--- TP0/TP0_SARINENA/TP3_Heroic_Fantasy_SARINENA/src/tp3_heroic_fantasy_sarinena/TP3_Heroic_Fantasy_SARINENA.java
+++ TP0/TP0_SARINENA/TP3_Heroic_Fantasy_SARINENA/src/tp3_heroic_fantasy_sarinena/TP3_Heroic_Fantasy_SARINENA.java
@@ -3,6 +3,10 @@ SARINENA, TDC , HEROIC FANTASY, 18/10/2023
  */
 package tp3_heroic_fantasy_sarinena;
 
+import Armes.Epee;
+import Armes.Arme;
+import Armes.Baton;
+import java.util.ArrayList; 
 /**
  *
  * @author pierr
@@ -13,7 +17,30 @@ public class TP3_Heroic_Fantasy_SARINENA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Epee epee_1 = new Epee("Excalibur", 7, 5);
+        Epee epee_2 = new Epee("Durandal", 4, 7);
+/*
+        System.out.println(epee_1);
+        System.out.println(epee_2);
+*/
+        Baton baton_1 = new Baton("Chene", 4, 5);
+        Baton baton_2 = new Baton("Charme", 5, 6);
+/*
+        System.out.println(baton_1);
+        System.out.println(baton_2);
+*/
+        ArrayList<Arme> Tab = new ArrayList<Arme>();
+        Tab.add(epee_1);
+        Tab.add(epee_2);
+        Tab.add(baton_1);
+        Tab.add(baton_2);
         
-    }
-    
+//Permet d'afficher toute les valeur du tableau
+
+        int taille = Tab.size(); //permet de connaitre la taille du tableau dynamique
+        for (int i = 0; i < taille; i++) {
+            Arme valeur = Tab.get(i);
+            System.out.println(valeur);
+            }
+        }   
 }
