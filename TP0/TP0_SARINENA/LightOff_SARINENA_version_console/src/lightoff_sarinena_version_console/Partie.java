d2c2928ee7b25c23ee223b34aa2da625a237e8ff
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-11-04 10:22:38 +0100

    LightOff_jusqu'à fin de jeu complet avec quelques améliorations

--- TP0/TP0_SARINENA/LightOff_SARINENA_version_console/src/lightoff_sarinena_version_console/Partie.java
+++ TP0/TP0_SARINENA/LightOff_SARINENA_version_console/src/lightoff_sarinena_version_console/Partie.java
@@ -16,62 +16,80 @@ public class Partie {
     GrilleDeJeu grille;
     int nbCoups;
 
-    /**
-     * crée une nouvelle instance de la grille de cellules lumineuses, qui sera
-     * dès lors accessible via la référence grille ; en outre, il initialise le
-     * compteur nbCoups à zéro
-     */
     public Partie() {
-        this.grille = new GrilleDeJeu(7, 7);
+        this.grille = new GrilleDeJeu(7, 7); // Taille par défaut
         this.nbCoups = 0;
     }
 
-    public void initialiserPartie() {
+    public void initialiserPartie(int largeur, int hauteur) {
+        grille = new GrilleDeJeu(largeur, hauteur);
         grille.melangerMatriceAleatoirement(10);
         nbCoups = 0;
     }
 
     public void LancerPartie() {
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenue et c'est l'heure de jouer à LightOff :");
-        initialiserPartie();
+        System.out.println("Choisir la difficulté de la partie : \n 1) Facile \n 2) Moyen \n 3) Difficile");
+        int choixDifficulte = sc.nextInt();
 
-        while (grille.cellulesToutesEteintes() == false) {
-            System.out.println("A votre tour, choissisez qu'est-ce que vous voulez modifier:");
-            System.out.println("1) Une Ligne");
-            System.out.println("2) Une Colonne");
-            System.out.println("3) Une diagonale Descendante");
-            System.out.println("4) Une Diagonale Montante");
-            System.out.println(grille);
-            int modif = sc.nextInt();
+        int largeur, hauteur;
+        int nbCoupsMax = 0;
+        switch (choixDifficulte) {
+            case 1:
+                largeur = 4;
+                hauteur = 4;
+                nbCoupsMax = 100;
+                break;
+            case 2:
+                largeur = 7;
+                hauteur = 7;
+                nbCoupsMax = 15;
+                break;
+            case 3:
+                largeur = 10;
+                hauteur = 10;
+                nbCoupsMax = 8;
+                break;
+            default:
+                System.out.println("Difficulté invalide. Choisissez une option valide.");
+                return;
+        }
 
-            if (modif == 1) {
-                System.out.println("Choissisez votre ligne à modifier");
-                int numL = sc.nextInt();
-                grille.activerLigneDeCellules(numL);
+        initialiserPartie(largeur, hauteur);
+
+            while (nbCoups < nbCoupsMax && !grille.cellulesToutesEteintes()) {
+                System.out.println("A votre tour, choisissez ce que vous voulez modifier :");
+                System.out.println("1) Une Ligne");
+                System.out.println("2) Une Colonne");
+                System.out.println("3) Une diagonale Descendante");
+                System.out.println("4) Une Diagonale Montante");
                 System.out.println(grille);
-                nbCoups++;
+                int modif = sc.nextInt();
 
-            } else if (modif == 2) {
-                System.out.println("Choissisez votre colonne à modifier");
-                int numC = sc.nextInt();
-                grille.activerColonneDeCellules(numC);
-                System.out.println(grille);
+                if (modif == 1) {
+                    System.out.println("Choisissez le numero de la ligne à modifier :");
+                    int num = sc.nextInt();
+                    grille.activerLigneDeCellules(num);
+                } else if (modif == 2) {
+                    System.out.println("Choisissez le numero de la colonne à modifier :");
+                    int num = sc.nextInt();
+                    grille.activerColonneDeCellules(num);
+                } else if (modif == 3) {
+                    grille.activerDiagonaleDescendante();
+                } else if (modif == 4) {
+                    grille.activerDiagonaleMontante();
+                } else {
+                    System.out.println("Option invalide. Choisissez une option valide.");
+                    continue;
+                }
                 nbCoups++;
-
-            } else if (modif == 3) {
-                grille.activerDiagonaleDescendante();
-                System.out.println(grille);
-                nbCoups++;
-
             }
-            if (modif == 4) {
-                grille.activerDiagonaleMontante();
-                System.out.println(grille);
-            }
-            nbCoups++;
+        if (grille.cellulesToutesEteintes()) {
+            System.out.println("Félicitations, vous avez gagné en " + nbCoups + " coups !");
+        } else {
+            System.out.println("Désolé, vous avez dépassé le nombre maximum de coups. La partie est terminée.");
         }
     }
-
 }
+
