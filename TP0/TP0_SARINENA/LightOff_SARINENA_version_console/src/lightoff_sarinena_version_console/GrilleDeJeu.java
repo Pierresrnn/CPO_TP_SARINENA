0c6645b37029f2553bf3dc36f6fff6051fc4a262
Pierre SARINENA <pierre.sarinena@epfedu.fr>
2023-11-07 12:54:16 +0100

    LightOff avec fin

--- TP0/TP0_SARINENA/LightOff_SARINENA_version_console/src/lightoff_sarinena_version_console/GrilleDeJeu.java
+++ TP0/TP0_SARINENA/LightOff_SARINENA_version_console/src/lightoff_sarinena_version_console/GrilleDeJeu.java
@@ -109,7 +109,7 @@ public class GrilleDeJeu {
      * @param idLigne correspond a la ligne choisie
      */
     public void activerLigneDeCellules(int idLigne) {
-        for (int k = 0; k < idLigne; k++) {
+        for (int k = 0; k < this.nbColonnes; k++) {
             this.matriceCellule[idLigne][k].activerCellule();
         }
     }
@@ -120,7 +120,7 @@ public class GrilleDeJeu {
      * @param idColonne correspond a la colonne choisie
      */
     public void activerColonneDeCellules(int idColonne) {
-        for (int l = 0; l < idColonne; l++) {
+        for (int l = 0; l < this.nbLignes; l++) {
             this.matriceCellule[l][idColonne].activerCellule();
         }
     }
